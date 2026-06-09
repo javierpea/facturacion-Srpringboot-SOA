b4c8ec82c0d236d85b18922d3b6fe1f065942f16
Hlagua <hlagua6116@uta.edu.ec>
2026-06-09 14:06:03 -0500

    areglo creacion sucursal

--- src/main/java/com/empresa/sistema_facturacion/entity/Sucursal.java
+++ src/main/java/com/empresa/sistema_facturacion/entity/Sucursal.java
@@ -10,7 +10,7 @@ public class Sucursal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false,length = 50)
     private String nombre;
